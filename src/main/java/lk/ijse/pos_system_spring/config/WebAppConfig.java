a6203dac27a66fbc2ea54151cb973502fd993a01
sangeeth <ssulochanaherath@gmail.com>
2024-10-14 23:11:24 +0530

    WebAppConfig class created.

---
+++ src/main/java/lk/ijse/pos_system_spring/config/WebAppConfig.java
@@ -0,0 +1,17 @@
+package lk.ijse.pos_system_spring.config;
+
+import jakarta.servlet.annotation.MultipartConfig;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.pos_system_spring")
+@EnableWebMvc
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 2,
+        maxFileSize = 1024 * 1024 * 5,
+        maxRequestSize = 1024 * 1024 * 10
+)
+public class WebAppConfig {
+}
