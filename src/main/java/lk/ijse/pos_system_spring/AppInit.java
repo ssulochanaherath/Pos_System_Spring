87b248a8e1b5a3a8273bdd0545ddf9aa1009bde4
sangeeth <ssulochanaherath@gmail.com>
2024-10-14 23:08:50 +0530

    AppInit class created.

---
+++ src/main/java/lk/ijse/pos_system_spring/AppInit.java
@@ -0,0 +1,20 @@
+package lk.ijse.pos_system_spring;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
